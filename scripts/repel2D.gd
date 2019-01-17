d40a199154477c5f05e2eec705e4ec59d6a1e7d3
Jordan <jordan.conwaymclaughlin24@mail.dcu.ie>
2019-01-17 21:13:19 +0000

    implemented untested fanning raycast system

--- scripts/repel2D.gd
+++ scripts/repel2D.gd
@@ -1,25 +1,66 @@
-extends Node2D
+extends Area2D
 
-export (float) var radius = 10
-export (float, 0, 360, 1) var angle = 60
-export (float, 0, 360, 1) var normal = 0
-export (int) var resolution = 16
+export (float) var radius = 7 setget _set_radius
+export (float, 0, 360, 1) var angle = 70 setget _set_angle
+export (float, 0, 360, 1) var normal = 0 setget _set_normal # not yet implemented
+export (int, 0, 128) var resolution = 11 setget _set_resolution
+
+var layers = []
+
+var force = Vector2()
+
+func _set_radius(f):
+	radius = f
+	pass
+func _set_angle(f):
+	angle = f
+	pass
+func _set_normal(f):
+	normal = f
+	pass
+func _set_resolution(i):
+	resolution = i
+	pass
 
 func get_points():
 	var out = []
-	for i in range(resolution):
-		var a = deg2rad(normal + angle * float(i) / float(resolution))
+	for i in range(resolution / 2):
+		var prog = (2 * (i+1) - (resolution - 1) % 2) / float(resolution - 1)
+		var a = deg2rad(normal + angle/2 * prog)
 		var point = Vector2(sin(a), cos(a)) * radius
 		out.append(point)
 		point.x *= -1
 		out.append(point)
+	
+	if resolution % 2 != 0:
+		out.append(Vector2(0, radius))
 	return out
 
+func get_space_state():
+	return get_world_2d().direct_space_state
+
+func ray_from_centre(endpoint, space_state=get_space_state()):
+	var sq_dist = endpoint.length_squared()
+	var out
+	for i in layers:
+		var ray =  space_state.intersect_ray(
+			global_position,
+			global_position + endpoint,
+			[],
+			i)
+		if 'position' in ray:
+			var sq_dist0 = (ray['position'] - global_position).length_squared()
+			if sq_dist0 < sq_dist:
+				out = ray
+				sq_dist = sq_dist0
+	return out
+
+func get_footing():
+	var space_state = get_world_2d().direct_space_state #documentation recommends calling this function continually but I'm not sure why
+	for v in get_points():
+		var ray = ray_from_centre(v, space_state)
+		if ray: return ray
+
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-func _physics_process(delta):
-
-	pass
+	for i in range(32):
+		if get_collision_layer_bit(i): layers.append(i)
--- scripts/repel2D_debug.gd
+++ scripts/repel2D_debug.gd
@@ -1,11 +1,33 @@
 tool
 extends 'res://scripts/repel2D.gd'
 
-const col = Color('ff0000')
+const col = Color(1,0,0,0.3)
+
+func redraw():
+	if Engine.editor_hint:
+		update()
+
+func _set_radius(f): 
+	._set_radius(f)
+	redraw()
+func _set_angle(f):
+	._set_angle(f)
+	redraw()
+func _set_normal(f): 
+	._set_normal(f)
+	redraw()
+func _set_resolution(i):
+	._set_resolution(i)
+	redraw()
 
 func _draw():
-	if not Engine.editor_hint:
+	if Engine.editor_hint:
 		for v in get_points():
-			draw_line(position, position+v, col)
+			draw_line(Vector2(0,0), v, col)
+	pass
 
+func _ready():
+	pass
+
+func _physics_process(delta):
 	pass
