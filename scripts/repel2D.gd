184f76b127301f7cd4060f6ca76ebce99bb9dfc6
Jordan <jordan.conwaymclaughlin24@mail.dcu.ie>
2019-01-17 21:18:34 +0000

    simplified fanning raycast system

--- scripts/repel2D.gd
+++ scripts/repel2D.gd
@@ -5,8 +5,6 @@ export (float, 0, 360, 1) var angle = 70 setget _set_angle
 export (float, 0, 360, 1) var normal = 0 setget _set_normal # not yet implemented
 export (int, 0, 128) var resolution = 11 setget _set_resolution
 
-var layers = []
-
 var force = Vector2()
 
 func _set_radius(f):
@@ -40,27 +38,14 @@ func get_space_state():
 	return get_world_2d().direct_space_state
 
 func ray_from_centre(endpoint, space_state=get_space_state()):
-	var sq_dist = endpoint.length_squared()
-	var out
-	for i in layers:
-		var ray =  space_state.intersect_ray(
-			global_position,
-			global_position + endpoint,
-			[],
-			i)
-		if 'position' in ray:
-			var sq_dist0 = (ray['position'] - global_position).length_squared()
-			if sq_dist0 < sq_dist:
-				out = ray
-				sq_dist = sq_dist0
-	return out
+	return space_state.intersect_ray(
+		global_position,
+		global_position + endpoint,
+		[],
+		collision_layer)
 
 func get_footing():
 	var space_state = get_world_2d().direct_space_state #documentation recommends calling this function continually but I'm not sure why
 	for v in get_points():
 		var ray = ray_from_centre(v, space_state)
-		if ray: return ray
-
-func _ready():
-	for i in range(32):
-		if get_collision_layer_bit(i): layers.append(i)
+		if 'position' in ray: return ray
