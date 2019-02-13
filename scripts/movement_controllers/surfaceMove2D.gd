01e331fb28385d1abefd6b54300d79303477614e
Jordan <jordan.conwaymclaughlin24@mail.dcu.ie>
2019-02-13 15:57:11 +0000

    fixed some oversights and temporarily removed directional agnosticicty

--- scripts/movement_controllers/surfaceMove2D.gd
+++ scripts/movement_controllers/surfaceMove2D.gd
@@ -5,40 +5,38 @@ extends KinematicBody2D
 #and movement speed is determined by the friction on that surface and the force acting on the body towards that surface
 #implemented properly it should provide an elegant mathematical solution for walking, sliding wallrunning, walljumping, etc.
 
-const FORCES = Vector2(0,200) #this should be inherited or read from utility in future iterations
+const FORCES = Vector2(0,100) #this should be inherited or read from utility in future iterations
 
 export (float) var topSpeed = 120 #maximum speed (decimeters?)
 export (float) var damping = 1 #time (seconds) to reach top speed
-export (float) var repulsion = 2
+export (float) var repulsion = 400
 
 const CUTOFF = 0.1 #acceptable threshold to target speed
 var velocity = Vector2()
-var walk = 0.0
-
+var walk = 0.0	
+	
 func _physics_process(delta):
 	velocity += FORCES * delta
-
 	var foot_psh
 	var foot_dir
 
 	var footing = $foot.get_footing()
 	if footing:
 		var foot_pos = $foot.global_position - footing['position']
-		foot_psh = foot_pos.length() * foot_pos.normalized() * repulsion * inMap.move_y
-		velocity += foot_psh
+		var foot_dst = foot_pos.length() / $foot.radius
+	
+		foot_psh = (1 - foot_dst) * repulsion * inMap.move_y
+		velocity.y -= foot_psh * delta
 
-		print(footing.normal)
-
-
-	var walkTarg = inMap.move_x * topSpeed
-	if 0 < damping:
-		var dWalk = walkTarg - velocity.x
-		if CUTOFF < abs(dWalk): 
-			velocity.x += sign(dWalk) * topSpeed * delta / damping
+		var walkTarg = inMap.move_x * topSpeed
+		if 0 < damping:
+			var dWalk = walkTarg - velocity.x
+			if CUTOFF < abs(dWalk): 
+				velocity.x += sign(dWalk) * topSpeed * delta / damping
+			else:
+				velocity.x = walkTarg
 		else:
 			velocity.x = walkTarg
-	else:
-		velocity.x = walkTarg
-	#not that movement is implemented directly rather than with forces. This allows for more direct control and responsiveness but may need to be changed if compaitibility issues mount
+	#note that movement is implemented directly rather than with forces. This allows for more direct control and responsiveness but may need to be changed if compaitibility issues mount
 	velocity = move_and_slide(velocity)
 	pass
